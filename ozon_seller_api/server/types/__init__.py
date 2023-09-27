4866ce9d3a858b8c9ca7878e1952e98b587533e1
pypchuk <opengl32.dll.tar.gz@gmail.com>
2023-09-27 14:30:40 +0300

    Add MessageType to __all__

--- ozon_seller_api/server/types/__init__.py
+++ ozon_seller_api/server/types/__init__.py
@@ -1,10 +1,12 @@
 from .new_posting import NewPosting
 from .state_changed import StateChanged
 from .ping import Ping
+from .message_type import MessageType
 
 
 __all__ = [
     "Ping",
     "NewPosting",
-    "StateChanged"
+    "StateChanged",
+    "MessageType",
 ]
